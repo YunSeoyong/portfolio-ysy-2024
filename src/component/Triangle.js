be89b7baefb8de318023fa6c24678b8b8baf24be
YunSeoyong <seoy2515@gmail.com>
2024-02-26 09:06:54 +0900

    스킬 이벤트

--- src/component/Triangle.js
+++ src/component/Triangle.js
@@ -29,7 +29,8 @@ const Triangle = ({ id, rotate, name, text, mouseoverTri, mouseoutTri, isModal }
                 d="M125.2 0.199951L0.199951 217.2H250.2L125.2 0.199951Z" fill={isModal ? '#74BBEF' : 'none'}
                 variants={tri}
                 initial="hidden"
-                animate="visible"
+                // animate="visible"
+                whileInView="visible"
                 transition={{
                     default: { duration: 2, ease: "easeInOut" },
                     fill: { duration: 2, ease: [1, 0, 0.8, 1] }
